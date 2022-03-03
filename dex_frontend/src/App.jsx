b3d2cfa2765082f6731bedfac2dbb7fb95ecacc8
Daniel Thompson <dthompson0190@gmail.com>
2022-03-03 08:25:41 -0600

    added header and universal header styling

---
+++ dex_frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css" //npm install bootstrap
+import "./css/Reset.css"
+import "./css/Style.css"
+import Header from "./Header"
+
+function App() {
+	return (
+		<>
+			<Header />
+		</>
+	)
+}
+
+export default App
---
+++ dex_frontend/src/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+	return (
+		<header className="main">
+			{/* DexApp Logo */}
+			<Link to="/" className="header-logo" />
+			{/* Desktop Navigation */}
+			<ul className="main-nav">
+				{/* <li>{searchBtn}</li> */}
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/dex">Pokedex</Link>
+				</li>
+				<li className="bold">
+					<Link to="/register">Sign Up</Link>
+				</li>
+				<li className="bold">
+					<Link to="/login">Login</Link>
+				</li>
+			</ul>
+		</header>
+	)
+}
+
+export default Header
