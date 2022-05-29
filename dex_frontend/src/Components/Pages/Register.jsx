97f16fa33a7a477c346bdaa4a39c5bcf698621df
Daniel Thompson <dthompson0190@gmail.com>
2022-05-28 20:20:13 -0500

    button is now disabled based on whether or not there are errors and input fields are not empty

--- dex_frontend/src/Components/Pages/Register.jsx
+++ dex_frontend/src/Components/Pages/Register.jsx
@@ -1,6 +1,8 @@
 import FormInput from "../Forms/FormInput"
+import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
-import { validator } from "../../Helpers/Validator"
+import { validator, checkIfValues, checkIfEmpty } from "../../Helpers/Validator"
+import pk_ball from "../../media/pokeball.png"
 
 const Register = () => {
 	const [values, setValues] = useState({
@@ -10,21 +12,14 @@ const Register = () => {
 		password: "",
 		confirm: "",
 	})
-	const [errors, setErrors] = useState({
-		name: "",
-		username: "",
-		email: "",
-		password: "",
-		confirm: "",
-	})
+	const [errors, setErrors] = useState(values)
+
 	const handleChange = (event) => {
 		setValues({ ...values, [event.target.id]: event.target.value })
 	}
-	const handleBlur = (event) => {
-		let newError = validator(event.target.id, event.target.value, values.password)
-		setErrors({ ...errors, ...newError })
-	}
-
+	useEffect(() => {
+		setErrors({ ...errors, ...validator(values) })
+	}, [values])
 	console.log(values)
 	console.log(errors)
 	return (
@@ -33,31 +28,15 @@ const Register = () => {
 				<h2 className="header1">Register</h2>
 				<hr />
 				<form>
-					<FormInput label="Name" name="name" value={values.name} handleChange={handleChange} handleBlur={handleBlur} error={errors.name} />
-					<FormInput
-						label="Username"
-						name="username"
-						value={values.username}
-						handleChange={handleChange}
-						handleBlur={handleBlur}
-						error={errors.username}
-					/>
-					<FormInput
-						label="Email"
-						name="email"
-						type="email"
-						value={values.email}
-						handleChange={handleChange}
-						handleBlur={handleBlur}
-						error={errors.email}
-					/>
+					<FormInput label="Name" name="name" value={values.name} handleChange={handleChange} error={errors.name} />
+					<FormInput label="Username" name="username" value={values.username} handleChange={handleChange} error={errors.username} />
+					<FormInput label="Email" name="email" type="email" value={values.email} handleChange={handleChange} error={errors.email} />
 					<FormInput
 						label="Password"
 						name="password"
 						type="password"
 						value={values.password}
 						handleChange={handleChange}
-						handleBlur={handleBlur}
 						error={errors.password}
 					/>
 					<FormInput
@@ -66,10 +45,23 @@ const Register = () => {
 						type="password"
 						value={values.confirm}
 						handleChange={handleChange}
-						handleBlur={handleBlur}
 						error={errors.confirm}
 					/>
+					<div className="btn-container">
+						<button className="btn primary" disabled={checkIfEmpty(errors) && checkIfValues(values) ? "" : "disabled"}>
+							Sign Up
+						</button>
+						<Link to="/login" className="btn secondary">
+							Log In
+						</Link>
+					</div>
 				</form>
+			</div>
+			<div className="tag-line">
+				<h2 className="header1">
+					Join a vast community of <span className="bold">Pokemon Trainers</span> from all over the world!
+				</h2>
+				<img src={pk_ball} alt="7 Pokeballs bunched together" />
 			</div>
 		</div>
 	)
--- dex_frontend/src/Helpers/Validator.js
+++ dex_frontend/src/Helpers/Validator.js
@@ -1,98 +1,71 @@
 function validateName(value) {
-	if (value) {
-		if (value.trim().length < 2) {
-			return "Name must be at least 2 characters"
-		} else if (value.trim().length > 30) {
-			return "Name can't be more than 30 characters"
-		} else {
-			return ""
-		}
+	if (value && value.trim().length < 2) {
+		return "Name must be at least 2 characters"
+	} else if (value.trim().length > 30) {
+		return "Name can't be more than 30 characters"
 	} else {
-		return "Name field is empty"
+		return ""
 	}
 }
 function validateUsername(value) {
-	if (value) {
-		if (value.trim().length < 5) {
-			return "Username must be at least 5 characters"
-		} else if (value.trim().length > 15) {
-			return "Name can't be more than 15 characters"
-		} else {
-			return ""
-		}
+	if (value && value.trim().length < 5) {
+		return "Username must be at least 5 characters"
+	} else if (value.trim().length > 15) {
+		return "Name can't be more than 15 characters"
 	} else {
-		return "Username field is empty"
+		return ""
 	}
 }
 function validateEmail(value) {
 	const regex = RegExp(
 		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	)
-	if (value) {
-		if (!regex.test(value)) {
-			return "Invalid email address"
-		} else {
-			return ""
-		}
+	if (value && !regex.test(value)) {
+		return "Invalid email address"
 	} else {
-		return "Email field is empty"
+		return ""
 	}
 }
 function validatePassword(value) {
-	if (value) {
-		if (value.length < 8) {
-			return "Password must be at least 8 characters"
-		} else if (value.length > 16) {
-			return "Password can't be more than 16 characters"
-		} else {
-			return ""
-		}
+	if (value && value.length < 8) {
+		return "Password must be at least 8 characters"
+	} else if (value.length > 16) {
+		return "Password can't be more than 16 characters"
 	} else {
-		return "Password field is empty"
+		return ""
 	}
 }
 function confirmPasswordMatch(value1, value2) {
-	if (value1) {
-		if (value1 !== value2) {
-			return "Passwords do not match"
-		} else {
-			return ""
-		}
+	if (value1 && value1 !== value2) {
+		return "Passwords do not match"
 	} else {
-		return "Please confirm password"
+		return ""
 	}
 }
 
-export function validator(name, value) {
-	let errors = {}
-	if (name === "name") {
-		errors[name] = validateName(value)
-	}
-	if (name === "username") {
-		errors[name] = validateUsername(value)
-	}
-	if (name === "email") {
-		errors[name] = validateEmail(value)
-	}
-	if (name === "password") {
-		errors[name] = validatePassword(value)
-	}
-	if (name === "confirm") {
-		console.log(arguments)
-		errors[name] = confirmPasswordMatch(value, arguments[2])
+export function validator(object) {
+	let errors = {
+		name: validateName(object.name),
+		username: validateUsername(object.username),
+		email: validateEmail(object.email),
+		password: validatePassword(object.password),
+		confirm: confirmPasswordMatch(object.confirm, object.password),
 	}
 	return errors
+}
+export function checkValue(value) {
+	return value !== null && value !== ""
 }
 export function checkIfErrors(object) {
 	return Object.keys(object).length === 0
 }
+//Returns true if every value in the object is empty
+export function checkIfEmpty(object) {
+	let values = Object.values(object)
+	return values.every((x) => x === null || x === "")
+}
+//Return true if every value in the object is not empty
 export function checkIfValues(object) {
 	let values = Object.values(object)
 	return values.every((x) => x !== null && x !== "")
 }
-// 	name: validateName(object.name),
-// 	username: validateUsername(object.username),
-// 	email: validateEmail(object.email),
-// 	password: validatePassword(object.password),
-// 	confirm: confirmPasswordMatch(object.confirm, object.password),
-// }
