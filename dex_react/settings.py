b485220dcad323db2fb5bbd43cd0a3c1fdbd27ae
Daniel Thompson <dthompson0190@gmail.com>
2022-06-18 12:02:35 -0500

    removed secret key

--- dex_react/settings.py
+++ dex_react/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ag^3i5k+^9cb-flz2o3q&3&c-i%-=b2bov-345!fhv5t5_%7dn'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
